da7b8dc60385cc905220a7f5244fe07c8d29d74c
Quill McConnell <quill8197@yahoo.com>
2019-05-31 16:26:17 -0400

    Added definitions in  an associative array

--- definition.php
+++ definition.php
@@ -1,4 +1,17 @@
 <?php
+$definitions = array(
+    "ajax" => "a Greek hero in the Trojan War who rescued the body of Achilles and killed himself out of jealousy 
+    when Odysseus was awarded the armor of Achilles.",
+
+    "button" => "a small disk or knob sewn on to a garment, either to fasten it by being pushed through a slit made 
+    for the purpose or for decoration.",
+
+    "load" => "a heavy or bulky thing that is being carried or is about to be carried.",
+
+    "submit" => "accept or yield to a superior force or to the authority or will of another person.",
+
+    "update" => "an act of bringing something or someone up to date, or an updated version of something."
+);
 
 $term = $_POST['term'];
-echo "<Strong>$term</Strong>: definition";
+echo "<Strong>$term</Strong>: $definitions[$term]";
